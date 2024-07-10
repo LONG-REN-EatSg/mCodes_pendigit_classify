fe5f8608bc173bcb9109b588a5579a0d18624915
ZHAOZhengyi-PI-ACS <ZHAOZhengyi-tgx@users.noreply.github.com>
2024-07-10 17:42:00 +0800

    upload batch, consolidate
    create pen_digit_identify_calc for calculation and plotting.

--- batch_pendigit_.m
+++ batch_pendigit_.m
@@ -7,3 +7,7 @@ rel_pen([strPath, '\N=5000_ptn_config_QuadProgramming_sedumi.ini']);
 rel_pen([strPath, '\ptn_config_QuadProgramming_sedumi_N=1000.ini']);
 
 rel_pen([strPath, '\ptn_config_GaussianBayes.ini']);
+
+stOutPenIdentify = rel_pen('N=5000_ptn_config_neural_network_LVQ_20Layer_Iter100_Rate0d001.ini');
+stOutPenIdentify = rel_pen('ptn_config_GaussianBayes.ini');
+stOutPenIdentify = rel_pen('ptn_config_QuadProgramming_sedumi_N=1000.ini');
